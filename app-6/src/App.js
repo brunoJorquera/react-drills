99975d342e2012b71563e4521249b302bfca974f
brunoJorquera <jorquera.b18@gmail.com>
2021-10-28 11:55:32 -0700

    commit for the day

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Todo from './Todo'
 
 export default class Input extends Component {
 render() {
     return (
         <div>
-          
+          <Todo />
         </div>
       )
   }
